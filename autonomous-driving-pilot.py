6f452b8efb05c596fe3c5b60c5b4c344f3d9e2e2
elifsevenn <svnelif34@gmail.com>
2022-08-20 23:24:10 +0300

    fixed file name

--- autonomous-driving-pilot.py
+++ autonomous-driving-pilot.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-img_path = "/Users/Elif/Desktop/udemy/opencv/roadd.jpg"
+img_path = "/roadd.jpg"
 img = cv2.imread(img_path)
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 (thresh,output2) = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
